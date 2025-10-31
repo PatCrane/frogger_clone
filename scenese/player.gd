b5a120d7cf64c5c628fdd770a9a0207ec709b979
Pat Crane <67291819+PatCrane@users.noreply.github.com>
2025-10-31 01:50:35 -0400

    Init

---
+++ scenese/player.gd
@@ -0,0 +1,9 @@
+extends CharacterBody2D
+
+var direction: Vector2 = Vector2(1,1)
+var speed: int = 150
+
+func _physics_process(_delta: float) -> void:
+	direction = Input.get_vector("left", "right", "up", "down")
+	velocity = direction * speed
+	move_and_slide()
